6b5c0f5785ab5360e32f04630f89b8e7cda2a778
abirke <a.b.birke@student.tudelft.nl>
2025-02-05 18:13:59 +0100

    Added projects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: 'class', 
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          light: '#ffffff', 
+          dark: '#000000', 
+        },
+        secondary: {
+          light: '#dadada', 
+          dark: '#a1a1aa', 
+        },
+        background: {
+          light: '#FFFFFF',
+          dark: '#000000',
+        },
+        text: {
+          light: '#ffffff',
+          dark: '#000000',
+        },
       },
       fontFamily: {
         sans: ["var(--font-sans)", "sans-serif"],
