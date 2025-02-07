3d5b8165a3af2d62a65d744ed2e27779252064ce
Attila Birke <biatbe007@gmail.com>
2025-02-07 11:44:22 +0100

    Added design area. Added mobile view,

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,8 +12,8 @@ const projects = [
     id: 2,
     name: 'Price App',
     imageUrl: '/N4C_Logo.png',
-    techStack: ['Next.js', 'PostgreSQL', 'Prisma', 'Tailwind'],
-    description: '',
+    techStack: ['Next.js', 'Prisma', 'PostgreSQL', 'Tailwind', 'NextAuth'],
+    description: 'Deployed a Next.js application on Vercel which Network4Cars currently use. The application lets users manage car data for different countries and automatically update car prices (daily) based on third-party data.',
     link: ''
   },
   {
@@ -24,20 +24,27 @@ const projects = [
     description: '',
     link: ''
   },
+  {
+    id: 4,
+    name: 'Discount aggregator',
+    imageUrl: '',
+    techStack: ['Flask', 'Next.js'],
+    description: 'Currently in works. Developing a web application for displaying clothing brand discounts. Instead of having to check every clothing brand\'s site for discounts, with this app you can see them at one place and compare them.',
+    link: ''
+  },
 ]
 
 export default function Projects() {
 
     return (
-        <div className="flex gap-x-4 font-sans max-h-60 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 font-sans mt-4">
             {projects.map((project) => (
             <div 
                 key={project.id}
-                className="group hover:cursor-pointer w-[33%]"
             >
-                <div className="flex flex-col overflow-hidden rounded-xl border border-zinc-400">
-                    <div className="flex items-center justify-between border-b border-gray-900/5 p-2 px-6 group-hover:bg-zinc-700/25 transition-all duration-700 ease-in-out">
-                        <div className="text-sm/6 font-medium text-zinc-200">{project.name}</div>
+                <div className="group flex flex-col overflow-hidden rounded-xl border border-zinc-400 hover:cursor-pointer">
+                    <div className="flex items-center justify-between p-2 px-6 group-hover:border-b border-zinc-400/70 group-hover:bg-zinc-700/25 transition-all duration-700 ease-in-out">
+                        <div className="text-[12px] sm:text-sm/6 font-medium text-zinc-200 ">{project.name}</div>
                         {project.imageUrl ?
                             <img
                                 alt={project.name}
@@ -50,7 +57,7 @@ export default function Projects() {
                             />
                         }
                     </div>
-                    <div id={project.id.toString()} className="flex flex-col justify-between gap-x-4 max-h-0 px-3 transition-all duration-700 ease-in-out group-hover:bg-zinc-700/25 group-hover:max-h-60 group-hover:py-2 group-hover:cursor-pointer">
+                    <div id={project.id.toString()} className="flex flex-col justify-between gap-x-4 max-h-0 px-3 transition-all duration-700 ease-in-out group-hover:bg-zinc-700/25 group-hover:max-h-80 group-hover:py-2 group-hover:cursor-pointer">
                         <dt className="text-zinc-200">
                             Tech stack: {project.techStack.join(", ")}
                         </dt>
