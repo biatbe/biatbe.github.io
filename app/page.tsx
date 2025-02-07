1ae52c06d7a001b2792edb721f519b851a33a878
Attila Birke <biatbe007@gmail.com>
2025-02-07 11:44:22 +0100

    Added projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,57 @@
+import Projects from "./projects/page";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 h-[100vh] border border-gray-800 rounded-lg my-4 bg-[#141414]">
-      <div className="h-[20vh]">
-          placeholder
-      </div>
+    <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 pt-40 h-[100vh]">
       <div className="mx-auto">
         <div className="font-sans flex flex-col gap-2 mx-8">
-          <h1 className="text-5xl">
-            Software Developer
-          </h1>
-          <span className="text-xl text-zinc-400">
-            I am Attila, new-grad software developer. Based in The Hague, Netherlands.
+          <div className="flex items-center gap-4">
+            <span className="inline-flex size-24 items-center justify-center rounded-full bg-gray-500">
+              <span className="text-xl font-medium text-white">TW</span>
+            </span>
+            <div>
+              <h1 className="text-5xl">
+                Attila Birke
+              </h1>
+              <h3 className="text-2xl">
+                Software Developer
+              </h3>
+            </div>
+          </div>
+          <span className="text-xl text-zinc-400 mt-6">
+            I am Attila, new-grad software developer. On the side, I do web designs.<br/>
+            Based in The Hague, Netherlands.
           </span>
           <div id="contacts" className="flex mt-6 gap-5">
-            <a>
-              <img src='/svgs/github-brands-solid.svg' width={50}/>
+            <a href="https://github.com/biatbe" target="_blank">
+              {/* <AiFillGithub size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
+              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+                GitHub
+              </span>
+            </a>
+            <a href="https://linkedin.com/in/attila-birke-927935223" target="_blank">
+              {/* <AiFillLinkedin size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
+              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+                LinkedIn
+              </span>
+            </a>
+            <a href="mailto:attilabirke@gmail.com">
+              {/* <AiOutlineMail size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
+              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+                Email
+              </span>
+            </a>
+            <a href="/Resume_2025.pdf" target="_blank">
+              {/* <AiOutlineMail size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
+              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+                Resume
+              </span>
             </a>
           </div>
+        </div>
+        <div className="font-sans flex flex-col gap-2 mx-8 mt-12">
+          <h2 className="text-xl text-[#ffffff]">Projects</h2>
+          <Projects />
         </div>
       </div>
     </div>
