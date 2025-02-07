a00c8b7d12ccc5259418ad6ffe7e5983a32a3e19
Attila Birke <biatbe007@gmail.com>
2025-02-07 11:44:20 +0100

    Added design area. Added mobile view,

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,58 @@
+import Designs from "./designs/page";
 import Projects from "./projects/page";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 pt-40 h-[100vh]">
+    <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 sm:pt-20 pt-10">
       <div className="mx-auto">
-        <div className="font-sans flex flex-col gap-2 mx-8">
+        <div className="font-sans flex flex-col gap-2 sm:mx-8 mx-2">
           <div className="flex items-center gap-4">
             <span className="inline-flex size-24 items-center justify-center rounded-full bg-gray-500">
               <span className="text-xl font-medium text-white">TW</span>
             </span>
             <div>
-              <h1 className="text-5xl">
+              <h1 className="text-2xl sm:text-5xl">
                 Attila Birke
               </h1>
-              <h3 className="text-2xl">
+              <h3 className="text-xl sm:text-2xl">
                 Software Developer
               </h3>
             </div>
           </div>
-          <span className="text-xl text-zinc-400 mt-6">
+          <span className="sm:text-xl text-zinc-400 mt-6">
             I am Attila, new-grad software developer. On the side, I do web designs.<br/>
             Based in The Hague, Netherlands.
           </span>
-          <div id="contacts" className="flex mt-6 gap-5">
+          <div id="contacts" className="flex mt-6 gap-5 sm:text-xl text-[#a1a1aa] ">
             <a href="https://github.com/biatbe" target="_blank">
-              {/* <AiFillGithub size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
-              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+              <span className="hover:cursor-pointer hover:text-[#313131]">
                 GitHub
               </span>
             </a>
             <a href="https://linkedin.com/in/attila-birke-927935223" target="_blank">
-              {/* <AiFillLinkedin size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
-              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+              <span className="hover:cursor-pointer hover:text-[#313131]">
                 LinkedIn
               </span>
             </a>
             <a href="mailto:attilabirke@gmail.com">
-              {/* <AiOutlineMail size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
-              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+              <span className="hover:cursor-pointer hover:text-[#313131]">
                 Email
               </span>
             </a>
             <a href="/Resume_2025.pdf" target="_blank">
-              {/* <AiOutlineMail size={40} className="text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer"/> */}
-              <span className="text-xl text-[#a1a1aa] hover:text-[#313131] hover:cursor-pointer">
+              <span className="hover:cursor-pointer hover:text-[#313131]">
                 Resume
               </span>
             </a>
           </div>
         </div>
-        <div className="font-sans flex flex-col gap-2 mx-8 mt-12">
-          <h2 className="text-xl text-[#ffffff]">Projects</h2>
+        <div className="font-sans flex flex-col gap-2 sm:mx-8 mx-2 mt-12">
+          <h2 className="sm:text-xl text-[#ffffff]">Projects</h2>
           <Projects />
+        </div>
+        <div className="font-sans flex flex-col gap-2 sm:mx-8 mx-2 mt-12">
+          <h2 className="sm:text-xl text-[#ffffff]">Designs</h2>
+          <Designs />
         </div>
       </div>
     </div>
